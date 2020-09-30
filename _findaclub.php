43fb9069ba3dabd56400648470bc0fcfa4769d65
mtyerdua <58461935+mtyerdua@users.noreply.github.com>
2020-09-30 11:27:48 +0200

    add main content

--- _findaclub.php
+++ _findaclub.php
@@ -1,1 +1,18 @@
-<?php
+<div>
+    <h2>FIND A CLUB</h2>
+    <a href="#">
+        <img src="https://via.placeholder.com/150x150" alt="//">
+    </a>
+
+    <a href="#">
+        <img src="https://via.placeholder.com/150x150" alt="//">
+    </a>
+
+    <a href="#">
+        <img src="https://via.placeholder.com/150x150" alt="//">
+    </a>
+
+    <img src="https://via.placeholder.com/400x400" alt="//">
+
+    <hr>
+</div>
--- _prices.php
+++ _prices.php
@@ -1,1 +1,19 @@
-<?php
+<div>
+    <H2>TARIFS</H2>
+    <ul>
+        <li>
+            <h3>FORMULE 1</h3>
+            <p>hhhhh</p>
+        </li>
+
+        <li>
+            <h3>FORMULE 1</h3>
+            <p>hhhhh</p>
+        </li>
+        <li>
+            <h3>FORMULE 1</h3>
+            <p>hhhhh</p>
+        </li>
+    </ul>
+    <hr>
+</div>
