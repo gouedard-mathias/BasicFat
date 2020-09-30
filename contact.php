487f29a2f0b4182ead4428b142a57f7bce6d0ab6
Geoffrey Goilot <geoffreygoilot@gmail.com>
2020-09-30 10:49:38 +0200

    add contact.php

---
+++ contact.php
@@ -0,0 +1,38 @@
+
+
+<form action="" method="">
+    <fieldset>
+        <legend>Nous sommes à votre écoute</legend>
+        <div>
+            <label for="user-name"> Nom</label>
+            <input type="text" id="user-name" name="user-name" required placeholder="Nom Prénom">
+        </div>
+        <div>
+            <label for="user_email"> Email</label>
+            <input type="email" id="user_email" name="user_email" required placeholder="@">
+        </div>
+        <div>
+            <label for="user_phone"> Téléphone</label>
+            <input type="number" id="user_phone" name="user_phone" required>
+        <div>
+            <label for="message_subject">Objet de votre message</label>
+            <select name="message_subject" id="message_subject">
+                <option>"Avoir plus de frites à la cantine"</option>
+                <option> "se plaindre des coachs"</option>
+                <option >"je souhaiterais payer moins cher</option>
+            </select>
+        </div>
+        </div>
+            <label for="user_message"> Message</label>
+            <input type="textarea" id="user_message" name="user_message" required placeholder="Votre message...">
+        </div>
+    </fieldset>
+</form>
+
+<div>
+    /* insérer image*/
+</div>
+
+
+
+
