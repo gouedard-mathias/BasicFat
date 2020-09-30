8ccdb4652dab05d4e1c2dfe042addeec420e73e9
GOUEDARD Mathias <m-gouedard40500@hotmail.com>
2020-09-30 10:39:00 +0200

    add _header.php

---
+++ _header.php
@@ -0,0 +1,11 @@
+<h1>Basic-Fat</h1>
+<nav class="navbar">
+    <ul>
+        <li>Trouver un club</li>
+        <li>Tarifs</li>
+        <li>Présentation</li>
+        <li>Coaching</li>
+        <li>Inscription</li>
+        <li>Contact</li>
+    </ul>
+</nav>
