5609246294a123ba06226a56f4b23781fc4a26e8
Geoffrey Goilot <geoffreygoilot@gmail.com>
2020-09-30 12:31:06 +0200

    add thanks.php

--- _contact.php
+++ _contact.php
@@ -1,10 +1,10 @@
 
-<form action="" method="">
+<form action="thanks.php" method="post">
     <fieldset>
         <legend>Nous sommes à votre écoute</legend>
         <div>
-            <label for="user-name"> Nom</label>
-            <input type="text" id="user-name" name="user-name" required placeholder="Nom Prénom">
+            <label for="user_name"> Nom</label>
+            <input type="text" id="user_name" name="user_name" required placeholder="Nom Prénom">
         </div>
         <div>
             <label for="user_email"> Email</label>
@@ -17,14 +17,17 @@
         <div>
             <label for="message_subject">Objet de votre message</label>
             <select name="message_subject" id="message_subject">
-                <option>"Avoir plus de frites à la cantine"</option>
-                <option> "se plaindre des coachs"</option>
-                <option >"je souhaiterais payer moins cher</option>
+                <option>Avoir plus de frites à la cantine
+                <option> Se plaindre des coachs car ils sont très très nuls
+                <option >Je souhaiterais payer moins cher
             </select>
         </div>
         <div>
         <label for="user_message"> Message </label>
             <textarea id="user_message" name="user_message" required rows="5" cols="33">Votre message...</textarea>
+        </div>
+        <div>
+            <button type="submit">Envoyer votre message</button>
         </div>
     </fieldset>
 </form>
