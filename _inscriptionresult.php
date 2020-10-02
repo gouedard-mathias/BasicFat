aa4a2b8e146a50157cb5a8b9d57391052038d1bf
GOUEDARD Mathias <m-gouedard40500@hotmail.com>
2020-10-02 09:54:05 +0200

    modify navbar

--- _inscriptionresult.php
+++ _inscriptionresult.php
@@ -6,17 +6,21 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Basic-Fat inscription terminé</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<?php include("_header.php") ?>
+    <header>
+        <?php include("_header.php") ?>
 
-<div class="container">
-    <p class="title"><?php echo $_GET['user_genre']; ?> <?php echo $_GET['user-firstname']; ?> <?php echo $_GET['user-lastname']; ?></p>
-    <p><?php echo $_GET['user_email']; ?></p>
-    <p><?php echo $_GET['user_phone']; ?></p>
-    <p>Votre inscription a bien été prise en compte</p>
-</div>
+    </header>
 
-<?php include("_footer.php") ?>
+    <div class="container">
+        <p class="title"><?php echo $_GET['user_genre']; ?> <?php echo $_GET['user-firstname']; ?> <?php echo $_GET['user-lastname']; ?></p>
+        <p><?php echo $_GET['user_email']; ?></p>
+        <p><?php echo $_GET['user_phone']; ?></p>
+        <p>Votre inscription a bien été prise en compte</p>
+    </div>
+
+    <?php include("_footer.php") ?>
 </body>
 </html>
