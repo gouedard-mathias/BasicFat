7f1d4a84959ac52f2956d21c1faf6418d40246c2
GOUEDARD Mathias <m-gouedard40500@hotmail.com>
2020-10-06 13:58:04 +0200

    modify navbar

--- _header.php
+++ _header.php
@@ -1,15 +1,22 @@
-<div>
-    <nav class="navbar">
-        <img src="https://i.postimg.cc/jjMgCjT1/logo-copie.png" alt="Logo" class="logo">
-        <ul>
-            <li>Trouver un club</li>
-            <li>Tarifs</li>
-            <li>Présentation</li>
-            <li>Coaching</li>
-            <li>
-                <a href="_inscriptionform.php">Inscription</a>
-            </li>
-            <li>Contact</li>
-        </ul>
+<label>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <label class="logo">
+            <img src="https://i.postimg.cc/jjMgCjT1/logo-copie.png" alt="Logo">
+        </label>
+            <ul>
+                <li>Trouver un club</li>
+                <li>Tarifs</li>
+                <li>Présentation</li>
+                <li>Coaching</li>
+                <li>
+                    <a href="_inscriptionform.php">Inscription</a>
+                </li>
+                <li>Contact</li>
+            </ul>
     </nav>
 </div>
+
