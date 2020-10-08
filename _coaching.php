d6da61eef06368614434a699ad614b2dd6ffac93
mtyerdua <58461935+mtyerdua@users.noreply.github.com>
2020-10-08 10:48:36 +0200

    Finalisation of Find a club section + coaches img

--- _coaching.php
+++ _coaching.php
@@ -1,16 +1,16 @@
-<div class=" inlineElements">
-    <div class="blockText blockCoaching">
+<div class=" inlineElements coachingStart">
+    <div class="blockText blockCoaching ">
         <h2 class="coachingTitle">COACHING</h2>
         <p class="coachText"> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." </p>
     </div>
     <div class="coachImg">
         <div class="coachFlex">
-            <img src="https://i.postimg.cc/s2D6jtkb/loup.jpg" alt="">
-            <img src="https://i.postimg.cc/s2D6jtkb/loup.jpg" alt="">
+            <img src="https://i.postimg.cc/1tPMFqTn/jojobf.png" alt="">
+            <img src="https://i.postimg.cc/ZK183Nvb/mattbf.png" alt="">
         </div>
         <div class="coachFlex">
-            <img src="https://i.postimg.cc/s2D6jtkb/loup.jpg" alt="">
-            <img src="https://i.postimg.cc/s2D6jtkb/loup.jpg" alt="">
+            <img src="https://i.postimg.cc/HLmbvCtF/yassbf.png" alt="">
+            <img src="https://i.postimg.cc/fTgT24x5/dreybff.png" alt="">
         </div>
     </div>
 </div>
--- _findaclub.php
+++ _findaclub.php
@@ -1,4 +1,4 @@
-<h2 class="findAClubTitle">FIND A CLUB</h2>
+<h2 class="findAClubTitle">FIND A CLUB (sandwich)</h2>
 
 <div class="map sandwich">
     <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11315.663213217911!2d-0.558319!3d44.843648!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5c7b1fb61f04f824!2sBasic-Fit%20Salles%20de%20Sport%20Bordeaux%20Bastide!5e0!3m2!1sfr!2sfr!4v1602061742852!5m2!1sfr!2sfr" width="600" height="500" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
@@ -6,15 +6,15 @@
 
 <div class="sandwich facImg">
     <a href="">
-        <img src="https://via.placeholder.com/150x150" alt="">
+        <img src="https://i.postimg.cc/Sspcb64T/noun-Sandwich-1020657.png" alt="">
     </a>
 
     <a href="">
-        <img src="https://via.placeholder.com/150x150" alt="">
+        <img src="https://i.postimg.cc/Sspcb64T/noun-Sandwich-1020657.png" alt="">
     </a>
 
     <a href="">
-        <img src="https://via.placeholder.com/150x150" alt="">
+        <img src="https://i.postimg.cc/Sspcb64T/noun-Sandwich-1020657.png" alt="">
     </a>
 </div>
 
